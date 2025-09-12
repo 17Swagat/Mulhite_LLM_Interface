92abaab5ed2fe0bb9a100a398588981a3f36b80d
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-12 15:55:07 +0530

    Reactflow: Adding nodes to the UI

--- src/app/(graph)/graph/page.tsx
+++ src/app/(graph)/graph/page.tsx
@@ -1,13 +1,33 @@
-import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge, Background, MiniMap } from '@xyflow/react';
-import '@xyflow/react/dist/style.css';
+import {
+  ReactFlow,
+  applyNodeChanges,
+  applyEdgeChanges,
+  addEdge,
+  Background,
+  MiniMap,
+  Node,
+  Edge,
+} from "@xyflow/react";
+import "@xyflow/react/dist/style.css";
 
-export default function Graph(){
-    return (
+const nodes: Node[] = [
+  {
+    id: "1",
+    position: { x: 0, y: 0 },
+    data: { label: "Node 1" },
+    style: { color: "red", backgroundColor: 'rgba(16, 211, 110, 1)' },
+  },
+  { id: "2", position: { x: 100, y: 100 }, data: { label: "Node 2" }, style: { color: "blue", backgroundColor: 'rgba(16, 211, 110, 1)' } },
+];
+
+const edges: Edge[] = [];
+
+export default function Graph() {
+  return (
     <div className="w-[85%] h-[90%] bg-gray-700  text-6xl">
-        <ReactFlow
-            fitView
-         >
-            <Background color='rgba(255, 255, 0)' size={3} />
-        </ReactFlow>
-    </div>)
+      <ReactFlow fitView nodes={nodes}>
+        <Background color="rgba(255, 255, 0)" size={3} />
+      </ReactFlow>
+    </div>
+  );
 }
