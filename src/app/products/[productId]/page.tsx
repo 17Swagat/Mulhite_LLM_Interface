a612471a45889e79c4c466ff7f558cbc96e51062
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-12 18:45:28 +0530

    Dynamic Routes -> Tutorial did: Using Received parameters. ❌ Which is wrong according to Copilot

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,8 +1,15 @@
-export default function ProductIdPage({params} : {params: {productId: string}}) {
+// export default function ProductIdPage({params} : {params: {productId: string}}) {
+
+// Tutorial: Uses Promise to handle params
+export default async function ProductIdPage({params} : {params: Promise<{productId: string}>}) {
+    
+    const productId = (await params).productId;
+
     return (
         <div className="h-screen w-screen flex flex-col gap-2 items-center justify-center bg-gradient-to-r from-blue-900 via-purple-400 to-blue-500">
             <h1 className="text-7xl">Product Details Page</h1>
-            <h2 className="text-5xl font-bold">Product {params.productId}</h2>
+            <h2 className="text-5xl font-bold">Product {productId}</h2>
+            {/* <h2 className="text-5xl font-bold">Product {params.productId}</h2> */}
         </div>
     )
 }
