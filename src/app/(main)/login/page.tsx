7b7b5ca691d1557e71d4e54248738d8f71ef3687
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-12 15:02:10 +0530

    Experimenting with routing in same page & to different page

---
+++ src/app/(main)/login/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+export default function Login(){
+    return (
+        <div className="flex flex-col items-center">
+            <h1>Login Page</h1>
+            <label htmlFor="username">
+                Username:
+                <input type="text" id="username" placeholder="Enter name" className="border-2" />
+            </label>
+            <Link href="/" className="bg-blue-500 text-white py-2 px-4 rounded hover:brightness-150 active:brightness-75">
+                Go Back
+            </Link>
+        </div>
+    );
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,21 @@
 import Link from "next/link";
 export default function Home() {
   return (
-    <div 
-      className=" text-5xl h-screen flex items-center justify-center bg-blue-900">
-      <Link href="/graph" className="bg-blue-500 text-white py-2 px-4 rounded hover:brightness-150 active:brightness-75">Click</Link>
+    <div className=" text-5xl h-screen flex items-center justify-center bg-blue-900">
+      <div className="flex gap-2">
+        <Link
+          href="/graph"
+          className="bg-blue-500 text-white py-2 px-4 rounded hover:brightness-150 active:brightness-75"
+        >
+          Click
+        </Link>
+        <Link
+          href="/login"
+          className="bg-red-500 text-white py-2 px-4 rounded hover:brightness-150 active:brightness-75"
+        >
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
-
