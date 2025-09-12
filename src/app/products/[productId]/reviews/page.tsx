c3e3e760140a75a5ee6b9f15235d58782efd9dbe
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 02:00:19 +0530

    extra page.tsx in reviews.. Not necessary tbh

---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,12 @@
+export default async function ProductReviewsPage(){
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <ul>
+                <li>Review 1</li>
+                <li>Review 2</li>
+                <li>Review 3</li>
+                <li>Review 4</li>
+            </ul>
+        </div>
+    );
+}
