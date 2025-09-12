b21edc085dd1960c741c073628e1fef6ab4b0192
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-12 16:22:22 +0530

    Reactflow: Hiding ReactFlow's Mark

--- src/app/(graph)/graph/page.tsx
+++ src/app/(graph)/graph/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect } from "react";
 import {
   ReactFlow,
   applyNodeChanges,
@@ -7,6 +9,7 @@ import {
   MiniMap,
   Node,
   OnNodesChange,
+  OnEdgesChange,
   Edge,
   Controls,
 } from "@xyflow/react";
@@ -17,16 +20,36 @@ const nodes: Node[] = [
     id: "1",
     position: { x: 0, y: 0 },
     data: { label: "Node 1" },
-    style: { color: "red", backgroundColor: 'rgba(16, 211, 110, 1)' },
+    style: { color: "red", backgroundColor: "rgba(16, 211, 110, 1)" },
   },
-  { id: "2", position: { x: 100, y: 100 }, data: { label: "Node 2" }, style: { color: "blue", backgroundColor: 'rgba(16, 211, 110, 1)' } },
+  {
+    id: "2",
+    position: { x: 100, y: 100 },
+    data: { label: "Node 2" },
+    style: { color: "blue", backgroundColor: "rgba(16, 211, 110, 1)" },
+  },
 ];
 
 const edges: Edge[] = [
-    {id: 'e1-2', source: '1', target: '2', animated: true, style: { stroke: 'red'}},
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+    animated: true,
+    style: { stroke: "red" },
+  },
 ];
 
 export default function Graph() {
+   useEffect(() => {
+    const target = Array.from(document.querySelectorAll("a")).find(
+      (el) => el.textContent.trim() === "React Flow"
+    );
+    if (target) {
+      target.style.display = "none";
+    }
+  }, []);
+
 
 
   return (
@@ -34,7 +57,8 @@ export default function Graph() {
     <div className="w-screen h-screen bg-gray-700  text-6xl rounded-2xl">
       <ReactFlow fitView nodes={nodes} edges={edges}>
         <Background color="rgba(255, 255, 0)" size={3} />
-        <Controls style={{ color: 'black', blockSize: '200px',padding: '10px', backgroundColor: 'darkcyan',  justifyContent: 'center' }} />
+        <Controls style={{ color: "black" }} />
+        <MiniMap style={{ backgroundColor: "pink" }} nodeColor={"black"} />
       </ReactFlow>
     </div>
   );
