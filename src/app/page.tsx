b37e35de5bfc6d95786759624c0534f64f2652f6
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-12 17:52:41 +0530

    Basic routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-blue-900 text-9xl">
       Hello
+      <Link href="/about" className="text-white bg-purple-400 p-2 rounded-[10px] ml-10 text-3xl">
+        About
+      </Link>
+      <Link href="/profile" className="text-white bg-purple-400 p-2 rounded-[10px] ml-10 text-3xl">
+        Profile
+      </Link>
     </div>
   );
 }
-
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+export default function Profile() {
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center bg-purple-900 ">
+            <h1>Profile Page</h1>
+            <p>This is the profile page of our application.</p> 
+
+            <Link
+                href="/"
+                className="text-white bg-purple-400 p-2 rounded-[10px] ml-10 text-3xl"
+            >
+                Home
+            </Link>
+        </div>
+    );
+}
