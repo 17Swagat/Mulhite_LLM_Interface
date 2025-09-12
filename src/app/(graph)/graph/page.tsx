a7b552a9fbb2e27d7b2fde049cb3394c66856053
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-12 16:50:44 +0530

    Reactflow: Basic code to add Edges

--- src/app/(graph)/graph/page.tsx
+++ src/app/(graph)/graph/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 // import { useEffect } from "react";
+import { useCallback } from "react";
+
 import {
     ReactFlow,
     Background,
@@ -9,6 +11,8 @@ import {
     Controls,
     useNodesState,
     useEdgesState,
+    Connection,
+    addEdge,
 } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 
@@ -25,6 +29,12 @@ const initialNodes: Node[] = [
         data: { label: "Node 2" },
         style: { color: "blue", backgroundColor: "rgba(16, 211, 110, 1)" },
     },
+    {
+        id: "3",
+        position: { x: 100, y: 200 },
+        data: { label: "Node 3" },
+        style: { color: "blue", backgroundColor: "rgba(16, 211, 110, 1)" },
+    },
 ];
 
 const initialEdges: Edge[] = [
@@ -37,17 +47,23 @@ const initialEdges: Edge[] = [
     },
 ];
 
+
 export default function Graph() {
     const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
     const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-    
+
+    const onConnect = useCallback((connection: Connection) => {
+        const edge:Edge = { ...connection, animated: true, id: `${edges.length + 1}`, style: { stroke: "red" } };
+        setEdges(prevEdges => addEdge(edge, prevEdges))
+    }, [setEdges]);
+
     return (
         // <div className="w-[85%] h-[90%] bg-gray-700  text-6xl rounded-2xl">
         <div className="w-screen h-screen bg-gray-700  text-6xl rounded-2xl">
-            <ReactFlow fitView nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange}>
+            <ReactFlow fitView nodes={nodes} edges={edges} onNodesChange={onNodesChange} onEdgesChange={onEdgesChange} onConnect={onConnect} >
                 <Background color="rgba(255, 255, 0)" size={3} />
                 <Controls style={{ color: "black" }} />
-                <MiniMap style={{ backgroundColor: "pink" }}/>
+                <MiniMap style={{ backgroundColor: "pink" }} />
             </ReactFlow>
         </div>
     );
