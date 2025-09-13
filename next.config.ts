077e8897c1ccfa339559107457e39d2eee296523
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 15:18:40 +0530

    Customizing Page Not found Page and dealing with Adding External Images to the site. Editing in the next.config.ts file in order for the those external images to be allowed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Allowing images from external domains:
+  images: {
+    // domains: ['img.freepik.com'],
+    remotePatterns: [
+      new URL('https://img.freepik.com/free-vector/hand-drawn-404-error_23-2147746234.jpg?semt=ais_incoming&w=740&q=80'), 
+      new URL('https://static.vecteezy.com/system/resources/previews/008/255/803/non_2x/page-not-found-error-404-system-updates-uploading-computing-operation-installation-programs-system-maintenance-a-hand-drawn-layout-template-of-a-broken-robot-illustration-vector.jpg')
+
+
+    ]//'https://img.freepik.com/*')]
+  },
 };
 
 export default nextConfig;
