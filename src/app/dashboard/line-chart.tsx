d17fc4b184d24d39ad1ba37cc52a1c0f8e4e58f9
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 16:04:16 +0530

    It is essential for a NextJS Path to have a page.tsx [if using typescript] its folder to activate the route

---
+++ src/app/dashboard/line-chart.tsx
@@ -0,0 +1,7 @@
+export default function LineChart() {
+    return (
+        <div className="w-full h-full flex justify-center items-center">
+            <h1 className="text-4xl">Line Chart Page</h1>
+        </div>
+    );
+}
