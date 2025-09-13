bea33a067eec20665c60428138985747b6c593db
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 16:06:52 +0530

    Works fine after adding page.tsx to the route [/dashboard]

--- src/app/dashboard/line-chart.tsx
+++ src/app/dashboard/line-chart.tsx
@@ -1,7 +1,7 @@
 export default function LineChart() {
     return (
         <div className="w-full h-full flex justify-center items-center">
-            <h1 className="text-4xl">Line Chart Page</h1>
+            <h1 className="text-7xl">Line Chart Page</h1>
         </div>
     );
 }
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import LineChart from "./line-chart";
+export default function LineChartPage() {
+    return (
+        <div className="h-screen w-screen flex flex-col items-center justify-center bg-purple-900 ">
+            <LineChart />
+        </div>
+    );
+}
