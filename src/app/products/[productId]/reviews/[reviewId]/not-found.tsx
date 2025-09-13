bbccff75da224a36f6e73df6263f87559b54b21a
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 15:40:45 +0530

    Cleaning...

--- src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.tsx
@@ -1,25 +1,13 @@
-// NOTE:
-// * The NotFound component does not support any props or arguments.
-//
-// Q: What about cases if I need to show different 404 pages message based on the route parameters?
-// A: For that we will rely on usePathname() hook from NextJS.
-
 'use client';
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-import path from "path";
 
 export default function NotFound() {
 
-    // let imgURL_linkNotFound = 'https://img.freepik.com/free-vector/hand-drawn-404-error_23-2147746234.jpg?semt=ais_incoming&w=740&q=80';
     let imgURL_linkNotFound = 'https://static.vecteezy.com/system/resources/previews/008/255/803/non_2x/page-not-found-error-404-system-updates-uploading-computing-operation-installation-programs-system-maintenance-a-hand-drawn-layout-template-of-a-broken-robot-illustration-vector.jpg';
     
     const pathname = usePathname();
-    console.log(typeof pathname); //  string
-    console.log(pathname); // e.g: "/products/5254/reviews/4"
-
-    let productId: string = pathname.split('/')[2]; // e.g: "5254"
-    let reviewId: string = pathname.split('/')[4]; // e.g: "4"
+    let productId: string = pathname.split('/')[2];
 
     return (
         <div className="w-screen h-screen flex flex-col justify-center items-center">
