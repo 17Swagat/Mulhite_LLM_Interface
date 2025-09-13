1b1fbad00a88740014652ad041baf5ea0896d7da
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 16:08:05 +0530

    Default export is Required!!

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import LineChart from "./line-chart";
+// default export is required for a page or layout
 export default function LineChartPage() {
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-purple-900 ">
