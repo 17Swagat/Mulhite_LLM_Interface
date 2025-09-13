bc5dfd1af37fc7659ffc1dbbbc03425d6dc879f4
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-13 18:02:11 +0530

    Route Groups. Here the parent folder does not come in the URL and helps in organizing code structure.

---
+++ src/app/(auth)/forget-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl">Forgot Password</h1>
+        </div>
+    );
+}
