90f95c8a708c324535f064c3c1dcc0017ac12ddb
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-14 17:55:19 +0530

    `replace` attribute inside the <Link> tag

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -18,7 +18,7 @@ export default function ProductsPage() {
                 </h2>
             <h2 className="text-5xl bg-blue-400">
 
-                <Link href='/products/3'>
+                <Link href='/products/3' replace>
                 Product 3
                 </Link>
             </h2>
