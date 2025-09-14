dfdcddd6aff00a60fd2b5e36f6c667019030c1bb
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-15 01:19:32 +0530

    Handling only params.

--- src/app/(main)/articles/[articleId]/page.tsx
+++ src/app/(main)/articles/[articleId]/page.tsx
@@ -4,12 +4,15 @@ type Props = {
     params: Promise<{articleId: string}>
 }
 
-export default function Articles({params} : {params: Promise<{articleId: string}>}) {
+// export default function Articles({params} : {params: Promise<{articleId: string}>}) {
+export default function Articles({params} : Props) {
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-pink-600 text-white gap-4">
             <h1 className="text-6xl">Articles</h1>
             <h2 className="text-4xl">Article in English</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At fuga esse cupiditate doloribus aliquam perferendis sint et molestias quis inventore nesciunt cum expedita non vero dignissimos sit voluptatem magni qui animi, dolorem sequi dolore eveniet. Ducimus non autem amet quidem neque repudiandae, fuga explicabo illum culpa quasi, cum magni excepturi.</p>
+            <p className="px-5 text-2xl" lang="en">Hello Everyone.</p> 
+            {/* //<!-- French --> */}
+            <p className="px-5 text-2xl" lang="fr">Bonjour tout le monde.</p> 
         </div>
     );
 }
