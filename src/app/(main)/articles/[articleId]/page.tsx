4eecfe670fb2ebade8aea1ac16c5dcb827dbd33e
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-15 01:22:40 +0530

    Search Params.

--- src/app/(main)/articles/[articleId]/page.tsx
+++ src/app/(main)/articles/[articleId]/page.tsx
@@ -1,18 +1,29 @@
 import Link from "next/link";
 
+// type Props = {
+//     params: Promise<{articleId: string}>
+// }
+
 type Props = {
-    params: Promise<{articleId: string}>
-}
+    params: Promise<{ articleId: string }>;
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
 
 // export default function Articles({params} : {params: Promise<{articleId: string}>}) {
-export default function Articles({params} : Props) {
+export default async function Articles({params, searchParams} : Props) {
+
+    const lang = (await searchParams).lang;
+
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-pink-600 text-white gap-4">
             <h1 className="text-6xl">Articles</h1>
             <h2 className="text-4xl">Article in English</h2>
+            {(lang == "en") ? 
             <p className="px-5 text-2xl" lang="en">Hello Everyone.</p> 
-            {/* //<!-- French --> */}
+            :
             <p className="px-5 text-2xl" lang="fr">Bonjour tout le monde.</p> 
+        }
+            {/* //<!-- French --> */}
         </div>
     );
 }
