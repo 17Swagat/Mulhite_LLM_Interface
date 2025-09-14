b5ea1608937d7d98132569dd855b3847fe66e7aa
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-15 01:34:04 +0530

    Handling params and search paras and learning about destructuring..urls..

--- src/app/(main)/articles/[articleId]/page.tsx
+++ src/app/(main)/articles/[articleId]/page.tsx
@@ -6,18 +6,20 @@ import Link from "next/link";
 
 type Props = {
     params: Promise<{ articleId: string }>;
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+    // searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+    searchParams: Promise<{ lang?: "en" | "fr" }>;
 };
 
 // export default function Articles({params} : {params: Promise<{articleId: string}>}) {
 export default async function Articles({params, searchParams} : Props) {
 
-    const lang = (await searchParams).lang;
+    const {articleId} = (await params);
+    const {lang} = (await searchParams); //?.lang;
 
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-pink-600 text-white gap-4">
             <h1 className="text-6xl">Articles</h1>
-            <h2 className="text-4xl">Article in English</h2>
+            <h2 className="text-4xl">Article in English: {articleId}</h2>
             {(lang == "en") ? 
             <p className="px-5 text-2xl" lang="en">Hello Everyone.</p> 
             :
