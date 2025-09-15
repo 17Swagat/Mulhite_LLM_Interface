4f71675b974bf64e35dd54426b67d3c8bb711379
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-15 16:46:16 +0530

    -----

--- src/app/(main)/articles/[articleId]/page.tsx
+++ src/app/(main)/articles/[articleId]/page.tsx
@@ -1,12 +1,4 @@
-// NOTE: "To access [params] and [search-params] inside a client component we use `use()` hook, since we can't use async/await in a Client Component."
-
-// NOTE: "While `page.tsx` has access to both Params & Search-Params"
-// "`layout.tsx` has access to only Params."
-
-'use client';
 import Link from "next/link";
-
-import { use } from "react";
 
 type Props = {
     params: Promise<{ articleId: string }>;
@@ -14,12 +6,12 @@ type Props = {
     searchParams: Promise<{ lang?: "en" | "fr" }>;
 };
 
-// export default function Articles({params} : {params: Promise<{articleId: string}>}) {
 // export default async function Articles({params, searchParams} : Props) {
-export default function Articles({params, searchParams} : Props) {
+export default async function Articles({params, searchParams} : Props) {
 
-    const {articleId} = use(params); //(await params);
-    const {lang} = use(searchParams); //(await searchParams); //?.lang;
+    const {articleId} = (await params);
+    const {lang} = (await searchParams); //?.lang;
+
 
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-pink-600 text-white gap-4">
@@ -28,9 +20,7 @@ export default function Articles({params, searchParams} : Props) {
             {(lang == "en") ? 
             <p className="px-5 text-2xl" lang="en">Hello Everyone.</p> 
             :
-            <p className="px-5 text-2xl" lang="fr">Bonjour tout le monde.</p> 
-        }
-            {/* //<!-- French --> */}
+            <p className="px-5 text-2xl" lang="fr">Bonjour tout le monde.</p> }
         </div>
     );
 }
