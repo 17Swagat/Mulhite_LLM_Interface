b7c0d9daecd9f118cb960d1a68d37e0b70aa7a4c
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-15 17:04:30 +0530

    Programatically navigating via: `useRouter()` from 'next/navigation'

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,15 +1,27 @@
+'use client';
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 export default function Blogs() {
+    const router = useRouter();
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center bg-green-900 gap-4">
             <h1>Blogs</h1>
             <p>This is the blogs page of our application.</p>
-            <Link
+            {/* <Link
                 href="/"
                 className="text-white bg-purple-400 p-2 rounded-[10px] ml-10 text-3xl"
             >
                 Home
-            </Link>
+            </Link> */}
+            <button
+                type="button"
+                onClick={()=>{
+                    router.push('/')
+                }}
+                className="text-white bg-purple-400 p-2 rounded-[10px] ml-10 text-3xl"
+            >
+                Home
+            </button>
 
             <Link
                 href="/blog/first"
