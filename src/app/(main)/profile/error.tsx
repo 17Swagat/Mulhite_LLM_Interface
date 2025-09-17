9d7a3762d36c64a4e7a812e7f3447b90547dc1ae
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-17 17:59:36 +0530

    Handling Client Side & Server Side Reloading..

--- src/app/(main)/profile/error.tsx
+++ src/app/(main)/profile/error.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useRouter } from "next/navigation";
+import { startTransition } from "react";
+
 /*
 About `error.tsx`:=>
 -> "It automatically wraps route segments and their nested children in a React Error Boundary."
@@ -11,11 +14,20 @@ About `error.tsx`:=>
 export default function ProfilePage_Error({error, reset}: {error: Error, reset: ()=> void}){
     // NOTE: "Here, the `re-try` function will attempt to re-render Client Side."
 
+    // Let's Try to hit server side Re-Trying:->
+    const router = useRouter()
+    const reload = ()=>{
+        startTransition(()=>{
+            router.refresh()
+            reset()
+        })
+    }
+
     return (
         <div className="w-screen h-screen bg-pink-400 flex flex-col justify-center items-center">
             <h1 className="text-5xl font-bold">Error - Occured</h1>
             <h2 className="text-4xl font-bold">{error.message}</h2>
-            <button type="button" onClick={()=>reset()} className="bg-green-700 text-2xl p-1 font-bold rounded-[10px]">Try Again</button>
+            <button type="button" onClick={()=>reload()} className="bg-green-700 text-2xl p-1 font-bold rounded-[10px]">Try Again</button>
         </div>
     );
 }
