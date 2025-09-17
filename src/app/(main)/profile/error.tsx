27137fbbd18d1d0c779e28978986ccdfdb456681
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-17 17:41:35 +0530

    Info about `error.tsx`

--- src/app/(main)/profile/error.tsx
+++ src/app/(main)/profile/error.tsx
@@ -1,8 +1,17 @@
 'use client';
-export default function ProfilePage_Error(){
+
+/*
+About `error.tsx`:=>
+-> "It automatically wraps route segments and their nested children in a React Error Boundary."
+-> "You can create custom error UIs for specific segments using the file-system hierarchy."
+-> "It isolates errors to affected segments while keeping the rest of your app functional."
+*/
+
+export default function ProfilePage_Error({error}: {error: Error}){
     return (
-        <div className="w-screen h-screen bg-pink-400 flex justify-center items-center">
+        <div className="w-screen h-screen bg-pink-400 flex flex-col justify-center items-center">
             <h1 className="text-5xl font-bold">Error - Occured</h1>
+            <h2 className="text-4xl font-bold">{error.message}</h2>
         </div>
     );
 }
