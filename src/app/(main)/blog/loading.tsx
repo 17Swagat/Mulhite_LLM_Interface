2fac7a10040a84789a37bc26ba7da2a043308ddc
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-17 15:45:17 +0530

    ...

--- src/app/(main)/blog/loading.tsx
+++ src/app/(main)/blog/loading.tsx
@@ -1,7 +1,8 @@
+import { LoadingCircle } from "@/components/3rdParty/loading";
 export default function Loading_BlogPage(){
     return (
         <div className="w-screen h-screen bg-pink-400 text-white flex justify-center items-center">
-            Loading...
+            <LoadingCircle/>
         </div>
     );
 }
