bee19388a0a41dc2910d4f9a3df0df3e7f05df3a
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 01:17:02 +0530

    Route Handlers -> Getting more to know about API routes

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function DashBoard(){
+    return (
+        <div className="w-screen h-screen bg-purple-600/30">
+            <h1 className="text-4xl">Route Handlers</h1>
+            <p className="text-2xl">Let's learn about Route Handlers</p>
+            <Link href={'/dashboard/api'} className="text-2xl bg-amber-500/70">
+                DashBoard API Call
+            </Link>
+        </div>
+    );
+}
