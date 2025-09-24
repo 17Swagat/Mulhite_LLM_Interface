0b98c717e5f8e8fca6f35a5c739f2d7e3f5d7308
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-24 17:32:25 +0530

    Intercepting Routes -> (..) 1 Level above -> @ME: Doing the prev experiment on this route

--- src/app/(main)/f3/page.tsx
+++ src/app/(main)/f3/page.tsx
@@ -1,11 +1,20 @@
+'use client'
+import { LoadingCircle } from "@/components/3rdParty/loading";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function F3_Page(){
+    const router = useRouter();
+    useEffect(()=>{
+        router.replace('/f1')
+    }, [])
 
     return (
         <div>
-            <h1 className="text-4xl">F3 Page</h1>
-            <Link href='/f1' className="bg-amber-600 text-3xl p-2">F1</Link>
+            {/* <h1 className="text-4xl">F3 Page</h1>
+            <Link href='/f1' className="bg-amber-600 text-3xl p-2">F1</Link> */}
+            <LoadingCircle/>
         </div>
     );
 }
