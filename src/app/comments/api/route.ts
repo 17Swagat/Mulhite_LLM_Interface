960510fdacfa15871eb8ee999f9beb6c896e660e
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 03:08:16 +0530

    Route Handler: Using NextRequest for better handling search Queries. Since in nextjs we can only have 1 GET() request in a single route.ts file, this way we could let a single GET() request function handle multiple scenerios, like in this case if no task-search-query parameter is given it will return the entire list of comments, and we give the specified task search parameter value it will return the no. of comments

--- src/app/comments/api/route.ts
+++ src/app/comments/api/route.ts
@@ -1,19 +1,26 @@
 import { comments } from "../data";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-    return Response.json(comments);
+export async function GET(request: NextRequest) {
+	const searchParams = request.nextUrl.searchParams;
+	const task = searchParams.get("task");
+	if (task === 'comments-count') {
+		return Response.json({
+			"comments-count": comments.length
+		});
+	}
+	return Response.json(comments);
 }
 
 export async function POST(request: Request) {
-    const req = await request.json()
-    const new_comment = {
-        id: (comments.length + 1),
-        text: req['text']
-    }
-    comments.push(new_comment)
-    return new Response(
-        JSON.stringify(new_comment), {
-        headers: { 'Content-Type': 'application/json' },
-        status: 201
-    })
+  const req = await request.json();
+  const new_comment = {
+    id: comments.length + 1,
+    text: req["text"],
+  };
+  comments.push(new_comment);
+  return new Response(JSON.stringify(new_comment), {
+    headers: { "Content-Type": "application/json" },
+    status: 201,
+  });
 }
