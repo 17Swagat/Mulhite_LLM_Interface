12abe7b193a284d170d984e776973a99f386047a
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-25 02:04:42 +0530

    Route Handler: POST Request 0.1

--- src/app/comments/api/route.ts
+++ src/app/comments/api/route.ts
@@ -1,5 +1,19 @@
 import { comments } from "../data";
 
-export async function GET(){
+export async function GET() {
     return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+    const req = await request.json()
+    const new_comment = {
+        id: (comments.length + 1),
+        text: req['text']
+    }
+    comments.push(new_comment)
+    return new Response(
+        JSON.stringify(new_comment), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 201
+    })
 }
