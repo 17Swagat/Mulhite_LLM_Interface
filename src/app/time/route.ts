e54b2cc9414fd4003702c92757d20935021d38d5
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 20:52:33 +0530

    ISR: Incremental static Regeneration => {Caching gets done here (in the given example every 10seconds)}

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -1,9 +1,9 @@
 export const dynamic = "force-static";
 
-// "NOTE: There is no caching during development. So, during development on refreshing/reload the [Browser] on this URL, we will see the new time on each reload"
-
-// "In order to visually see the caching happening. We will have to build the Project and go to this route in order to see the caching happening."
-// "pnpm build"
+// Implement Incremental Static Regeneration (ISR):=>
+// Next.js will invalidate the cache when a
+// request comes in, at most once every 10 seconds.
+export const revalidate = 10;
 
 export async function GET(){
     return Response.json({
