94b1090a551730f89406017df053dc0198dd27fc
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 18:32:20 +0530

    Reading Cookie in the Route Handler

--- src/app/profile/route.ts
+++ src/app/profile/route.ts
@@ -4,6 +4,11 @@ import { headers } from "next/headers";
 export async function GET(request: NextRequest){
     const headersList = await headers();
     console.log(headersList.get('Authorization'))
+
+    // To read the cookie in the route-handler
+    const theme = request.cookies.get('theme')
+    console.log(theme)
+
     return new Response('<h1> Profile API data </h1>', {headers: {
         'Content-Type': 'text/html; charset=utf-8',
         'Set-Cookie': ['theme=dark', 'version=1'].join(', ')
