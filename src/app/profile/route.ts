72795b6bdbdf6f0cfa39286125e77039bdb1f850
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 18:37:18 +0530

    Setting up cookies using NextJS's cookies() funciton

--- src/app/profile/route.ts
+++ src/app/profile/route.ts
@@ -1,16 +1,25 @@
 import {NextRequest } from "next/server";
 import { headers } from "next/headers";
+import { cookies } from "next/headers";
 
 export async function GET(request: NextRequest){
     const headersList = await headers();
     console.log(headersList.get('Authorization'))
 
     // To read the cookie in the route-handler
-    const theme = request.cookies.get('theme')
-    console.log(theme)
+    // #Way 1
+    // const theme = request.cookies.get('theme')
+    // console.log(theme)
+    
+    // #Way 2
+    // NextJS's cookies() funciton
+    // "Setting Up Cookie"
+    const cookieStore = await cookies()
+    cookieStore.set('resultsPerPage', '20')
 
     return new Response('<h1> Profile API data </h1>', {headers: {
         'Content-Type': 'text/html; charset=utf-8',
-        'Set-Cookie': ['theme=dark', 'version=1'].join(', ')
+        'Set-Cookie': 'theme=dark'
+        // 'Set-Cookie': ['theme=dark', 'version=1'].join(', ')
     }})
 }
