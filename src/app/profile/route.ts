8a5379ffe303e10f87d20303599f434e0120ab56
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 17:13:57 +0530

    Accessing API CALL Headers

---
+++ src/app/profile/route.ts
@@ -0,0 +1,7 @@
+import {NextRequest } from "next/server";
+
+export async function GET(request: NextRequest){
+    const requestHeaders = new Headers(request.headers) 
+    console.log(requestHeaders.get('Authorization'))
+    return new Response('Profile API data')
+}
