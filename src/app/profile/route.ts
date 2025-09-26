415344acfba6cc915d2f237f0aea6512fae9534d
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 17:17:34 +0530

    Accessing API CALL Headers -> Way 2: Using NextJS's headers() function

--- src/app/profile/route.ts
+++ src/app/profile/route.ts
@@ -1,7 +1,13 @@
 import {NextRequest } from "next/server";
+import { headers } from "next/headers";
 
 export async function GET(request: NextRequest){
-    const requestHeaders = new Headers(request.headers) 
-    console.log(requestHeaders.get('Authorization'))
+    // Way #1
+    // const requestHeaders = new Headers(request.headers) 
+    // console.log(requestHeaders.get('Authorization'))
+
+    // Way #2
+    const headersList = await headers();
+    console.log(headersList.get('Authorization'))
     return new Response('Profile API data')
 }
