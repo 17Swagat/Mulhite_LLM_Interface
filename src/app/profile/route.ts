080a0766f6cce636df3de85adeae88e81d1900f3
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 18:26:38 +0530

    Setting Up a Cookie in the Header (1 cookie)

--- src/app/profile/route.ts
+++ src/app/profile/route.ts
@@ -2,15 +2,10 @@ import {NextRequest } from "next/server";
 import { headers } from "next/headers";
 
 export async function GET(request: NextRequest){
-    // Way #1
-    // const requestHeaders = new Headers(request.headers) 
-    // console.log(requestHeaders.get('Authorization'))
-
-    // Way #2
-    // Setting Outgoing Headers|:|==>
     const headersList = await headers();
     console.log(headersList.get('Authorization'))
     return new Response('<h1> Profile API data </h1>', {headers: {
-        'content-type': 'text/html; charset=utf-8'
+        'Content-Type': 'text/html; charset=utf-8',
+        'Set-Cookie': 'theme=dark'
     }})
 }
