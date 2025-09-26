5257eb20de30af450754efc3349c08ee790027b2
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 21:20:42 +0530

    If you're working using dynamic functions like `headers()` and `cookies()`, or working with the Request Object in your GET Method, caching won't be applied

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -7,6 +7,8 @@ export const revalidate = 10;
 
 // NOTE: "Caching only works with GET Methods. Other HTTP methods like PUT, PATCH or DELETE are never cached."
 
+// "If you're working using dynamic functions like `headers()` and `cookies()`, or working with the Request Object in your GET Method, caching won't be applied"
+
 export async function GET(){
     return Response.json({
         time: new Date().toLocaleTimeString()
