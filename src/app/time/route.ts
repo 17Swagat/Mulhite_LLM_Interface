b4aa8737a810de71d91d36d5750f76f07c5505a0
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-26 21:18:51 +0530

    Caching only works with GET Methods. Other HTTP methods like PUT, PATCH or DELETE are never cached.

--- src/app/time/route.ts
+++ src/app/time/route.ts
@@ -5,6 +5,8 @@ export const dynamic = "force-static";
 // request comes in, at most once every 10 seconds.
 export const revalidate = 10;
 
+// NOTE: "Caching only works with GET Methods. Other HTTP methods like PUT, PATCH or DELETE are never cached."
+
 export async function GET(){
     return Response.json({
         time: new Date().toLocaleTimeString()
