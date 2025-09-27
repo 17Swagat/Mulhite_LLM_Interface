cd0508986fca472b0e2b3d32fbc6edf358950eeb
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-27 16:45:10 +0530

    Middleware and cookies 0.1: Getting and setting cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,15 @@
-// Task: Let's Make a Middleware that redirects a user to the home page when he goes to the /profile URL
-
-// NOTE: 
-// "1 middleware file per Project."
-
 // Approach 01: Using Config Object
 // Approach 02: Using Conditions
+
+// Task: Use of [cookies] & [headers] in Middleware.
 
 import { NextResponse, NextRequest } from "next/server";
 
 export function middleware(request: NextRequest){
-    // Adding Multiple Routes :
-    if (['/profile', '/time2'].includes(request.nextUrl.pathname)){
-        // return NextResponse.redirect(new URL('/', request.url))
-        return NextResponse.rewrite(new URL('/dashboard', request.url))
+    const response = NextResponse.next();
+    const themePreference = request.cookies.get('theme')
+    if (!themePreference) {
+        response.cookies.set('theme', 'dark')
     }
+    return response;
 }
