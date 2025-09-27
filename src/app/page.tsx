b50d6fa7f96088c0f091baa3fbef327055afccfa
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-27 20:02:47 +0530

    ssr & handling cookies....

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function HomePage(){
     return (
-        <div className="w-screen h-screen bg-cyan-600/30">
-            <h1 className="text-4xl">Route Handlers</h1>
-            <p className="text-2xl">Let&apos;s learn about Route Handlers</p>
+        <div className="w-screen h-screen bg-purple-600/30">
+            <h1 className="text-4xl underline">Server and Client Components</h1>
+            <p className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id commodi dolor delectus minima voluptatem quasi consequatur asperiores quidem repellendus officia?</p>
         </div>
     );
 }
