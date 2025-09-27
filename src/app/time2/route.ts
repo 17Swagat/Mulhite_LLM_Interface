0a0fa95c7d41fa472048e4879c09fc47636362f6
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-27 17:41:54 +0530

    Server & Client Components..

--- src/app/time2/route.ts
+++
@@ -1,8 +0,0 @@
-// Turning "OFF" caching in order to see whether it actually happens or not:
-// export const dynamic = "force-static";
-
-export async function GET(){
-    return Response.json({
-        time: new Date().toLocaleTimeString()
-    });
-}
