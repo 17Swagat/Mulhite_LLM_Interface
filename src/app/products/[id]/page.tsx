04e225daa673a5eb7bcec3c68b5ff067621bfc93
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-28 21:24:42 +0530

    Dynamic Routes -> Once a static route is created, even though it get's updated every second even though they are present in the generateStaticParams() and once we go a new route that is not in the generateStaticParams() it creates a static page(.html, .rsc, .meta) for that route also which also gets update every second. [ISSUE]

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,5 +1,5 @@
 // Specify that dynamic routes should be rendered dynamically
-export const revalidate = 1; // Revalidate static pages every hour
+export const revalidate = 1; // Revalidate static pages every 1second (Even though the routes refreshs after 1 second, but once a route is generated, it will get store in the .next/sever/app/products/ directory and won't disappear. )
 export const dynamicParams = true;//'force-dynamic';
 
 // Static Generation for these specific paths(id):
