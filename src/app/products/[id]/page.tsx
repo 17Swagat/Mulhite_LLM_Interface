ba3a7d153bfbcc8b158dfabff424945bbe85c6d0
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-28 21:02:10 +0530

    Dynamic Routes -> With generateStaticParams(). Making the pages with the mentinoed routes inside the fucntion as static.

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,14 @@
+// Specify that dynamic routes should be rendered dynamically
+// export const revalidate = 1; // Revalidate static pages every hour
+// export const dynamicParams = true;//'force-dynamic';
+
+// Static Generation for these specific paths(id):
+export async function generateStaticParams() {
+    return [{id: '1'}, {id: '2'}, {id: '3'}];
+}
+
+
+
 export default async function ProductPage_Id({
   params,
 }: {
@@ -8,7 +19,7 @@ export default async function ProductPage_Id({
     
     return (
       <div className="w-screen h-screen bg-amber-700 flex items-center justify-center text-white text-3xl">
-        Product Page - {id}
+        Product Page - {id} -&gt; {new Date().toLocaleTimeString()}
       </div>
     );
 }
