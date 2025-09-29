7d48599a776f3455d2200c4d10cb6894f00dcc7a
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-29 15:37:14 +0530

    ...

--- src/app/server-only/page.tsx
+++ src/app/server-only/page.tsx
@@ -2,13 +2,14 @@ import { serverOnlyFunc } from "@/utils/serverOnlyFunction";
 import { clientOnlyFunc } from "@/utils/clientOnlyFunction";
 
 export default function ServerOnlyPage() {
-    // const serverValue = serverOnlyFunc();
-    const clientValue = clientOnlyFunc(); // ❌ This will cause a build error because a server component cannot import a client-only module
+    const serverValue = serverOnlyFunc();
+    // const clientValue = clientOnlyFunc(); // ❌ This will cause a build error because a server component cannot import a client-only module
     return <div className="w-screen h-screen">
         Server Only Page
         {/* <div>Server Value: {serverValue}</div> */}
         <div className="bg-pink-600/80">
-            Client Value: {clientValue}
+            {/* Client Value: {clientValue} */}
+            Server Value: {serverValue}
         </div>
     </div>
 }
