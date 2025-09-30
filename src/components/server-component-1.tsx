a615d2fa8f1852351e869c88d060795d2da054b2
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-30 16:53:43 +0530

    Client component inside a server component which is inside a server only page

--- src/components/server-component-1.tsx
+++ src/components/server-component-1.tsx
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import ServerComponent2 from './server-component-2';
+import ClientComponent1 from './client-component-1';
 
 export default function ServerComponent1() {
     // "Including a server only operation":=>
@@ -7,9 +8,10 @@ export default function ServerComponent1() {
     fs.readFileSync('src/components/server-component-1.tsx', 'utf-8');  // NOTE: Not important what file it is, just an example of server-side operation. We are not concered with the output of this function
 
     return (
-        <div className='bg-green-600'>
+        <div className='bg-green-600 p-3'>
             <h1>Server Component 1</h1>
             <ServerComponent2/>
+            <ClientComponent1/>
         </div>
     )
 }
