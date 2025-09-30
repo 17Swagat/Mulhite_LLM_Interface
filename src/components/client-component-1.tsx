327213fa913c0bc7e6d30f67ff310e3e0a80294b
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-30 16:49:33 +0530

    Client Component inside another client component which is inside a server only page

--- src/components/client-component-1.tsx
+++ src/components/client-component-1.tsx
@@ -1,9 +1,15 @@
 'use client';
 import { useState } from "react";
+import ClientComponent2 from "./client-component-2";
 
 export default function ClientComponent1() {
     // Including some sort of Client-Only Operation
     const [name, setName] = useState("Batman");
 
-    return <h1>Client Component 1: {name}</h1>
+    return (
+        <div className="bg-amber-700 p-3">
+            <h1>Client Component 1: {name}</h1>
+            <ClientComponent2/>
+        </div>
+    )
 }
--- src/components/client-component-2.tsx
+++ src/components/client-component-2.tsx
@@ -5,5 +5,9 @@ export default function ClientComponent2() {
     // Including some sort of Client-Only Operation
     const [name, setName] = useState("Superman");
 
-    return <h1>Client Component 2: {name}</h1>
+    return (
+        <div className="bg-cyan-800">
+            <h1>Client Component 2: {name}</h1>
+        </div>
+    )
 }
