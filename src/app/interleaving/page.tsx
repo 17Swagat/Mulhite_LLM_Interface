bfed97a774c0d00966b1a74d53dedb4e64225d22
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-09-30 17:07:47 +0530

    Trick: To use a Server Component inside a Client component, pass the Server Componenet as a children prop to the Client Component

--- src/app/interleaving/page.tsx
+++ src/app/interleaving/page.tsx
@@ -1,10 +1,10 @@
 import ClientComponent1 from "@/components/client-component-1";
-// import ServerComponent1 from "@/components/server-component-1";
+import ServerComponent1 from "@/components/server-component-1";
 
 export default function InterleavingPage() {
     return (<div className="bg-blue-400 p-3">
         <h1 className="underline">Interleaving Page</h1>
         {/* <ServerComponent1 /> */}
-        <ClientComponent1/>
+        <ClientComponent1 children={<ServerComponent1/>}/>
     </div>)
 }
--- src/components/client-component-1.tsx
+++ src/components/client-component-1.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import ClientComponent2 from "./client-component-2";
 import ServerComponent1 from "./server-component-1";
 
-export default function ClientComponent1() {
+export default function ClientComponent1({children}: {children: React.ReactNode}) {
     // Including some sort of Client-Only Operation
     const [name, setName] = useState("Batman");
 
@@ -11,7 +11,8 @@ export default function ClientComponent1() {
         <div className="bg-amber-700 p-3">
             <h1>Client Component 1: {name}</h1>
             <ClientComponent2/>
-            <ServerComponent1/>
+            {/* <ServerComponent1/> */}
+            {children}
         </div>
     )
 }
