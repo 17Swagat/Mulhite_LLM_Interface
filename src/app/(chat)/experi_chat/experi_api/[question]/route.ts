9a0f3fb98be35d8d4f2842e0070b057d71d6c359
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-07 09:38:33 +0530

    reference-stop: with generateText().

--- src/app/(chat)/experi_chat/experi_api/[question]/route.ts
+++ src/app/(chat)/experi_chat/experi_api/[question]/route.ts
@@ -9,13 +9,16 @@ const ollama = createOllama({
 export async function GET(req: NextRequest, 
     { params }: { params: Promise<{ question: string }> }) {
     const { question } = await params;
-    const { text } = await generateText({
+    // const { text } = await generateText({
+    const result = await generateText({
         model: ollama('deepseek-r1:1.5b'),
         prompt: question
     });
 
+    // console.log(result.reasoningText);
 
     return Response.json({
-        ai_message: text
+        // ai_reason: result.reasoningText,
+        ai_message: result.text
     })
 }
--- src/components/my/tempDataComponent.tsx
+++
@@ -1,7 +0,0 @@
-export async function DataComponent(){
-    const res = await fetch('http://localhost:3000/experi_chat/experi_api', { cache: 'no-store' });
-    const data = await res.text();
-    return <div className="bg-pink-400 text-black">
-    {data}
-    </div>;
-}
