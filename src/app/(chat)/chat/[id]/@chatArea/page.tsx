cbea0ba76a36d767921cafd82f63fc3794924f1d
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-09 00:34:58 +0530

    A basic go to sidebar on Sidebar Chat Click

---
+++ src/app/(chat)/chat/[id]/@chatArea/page.tsx
@@ -0,0 +1,36 @@
+// export default function NewChat(){
+//     return <div className="flex flex-col justify-center items-center">New Chat</div>
+// }
+
+import { loadChat } from "@/utils/chat-store";
+import { Error_ChatNotFound } from "@/utils/custom_errors/chat_errors";
+// import ChatNotFound from "./ChatNotFound";
+// import ChatArea from "./ChatArea";
+import ChatArea from "../ChatArea";
+import ChatNotFound from "../ChatNotFound";
+
+export default async function ChatPage_ID({ params }: { params: Promise<{ id: string }> }) {
+
+    const { id } = await params;
+
+    try {
+        const messages = await loadChat(id);
+        return <ChatArea id={id} initialMessages={messages} />;
+    } catch (error: unknown) {
+        if (error instanceof Error_ChatNotFound) {
+            return <ChatNotFound id={id} />;
+        }
+
+        // Optionally log unexpected errors
+        // console.error('Unexpected error:', error);
+        return (
+            <div className="flex flex-col justify-center items-center min-h-screen w-full bg-gray-900 text-white p-50 px-[200px]">
+                <h1 className="text-4xl font-bold mb-4">Chat Not Found</h1>
+                <p className="text-lg">Unexpected Error Occurred. Kindly Refresh.</p>
+            </div>
+        );
+    }
+
+
+
+}
--- src/app/(chat)/chat/[id]/layout.tsx
+++ src/app/(chat)/chat/[id]/layout.tsx
@@ -2,13 +2,17 @@ import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 // import { AppSidebar } from "@/components/app-sidebar"
 import { AppSidebar } from "./AppSidebar"
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ 
+  children, 
+  chatArea
+}: { children: React.ReactNode, chatArea: React.ReactNode }) {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="bg-pink-400 w-screen">
         <SidebarTrigger className="bg-green-400 m-2 fixed top-0" />
-        {children}
+        {chatArea}
+        {/* {children} */}
       </main>
     </SidebarProvider>
   )
