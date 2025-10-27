519f713f1ea85f9a44bea183653c0e913b844737
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-10-27 15:31:21 +0530

    Deleting unnecessary loading files

--- src/app/chat/_loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Chat_LoadingScreen() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
-            <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 border-t-blue-500 animate-spin h-32 w-32 mb-4"></div>
-            <h2 className="text-center text-xl font-semibold">Loading Chat...</h2>
-            <p className="w-1/3 text-center text-gray-600">Please wait while we set things up for you.</p>
-        </div>
-    )
-}
