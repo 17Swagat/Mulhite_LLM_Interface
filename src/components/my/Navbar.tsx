e409b2102550444cb052738f919fda0feb4e1517
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-11-15 22:38:09 +0530

    ...

--- src/components/my/Navbar.tsx
+++ src/components/my/Navbar.tsx
@@ -3,13 +3,13 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+// import {
+//   SignInButton,
+//   SignUpButton,
+//   SignedIn,
+//   SignedOut,
+//   UserButton,
+// } from "@clerk/nextjs";
 import { Menu, X } from "lucide-react"; // nice lightweight icons
 
 export default function Navbar() {
@@ -33,39 +33,7 @@ export default function Navbar() {
           <Link href="#pricing" className="hover:text-white transition">
             Pricing
           </Link>
-          {/* <Link href="/tutorial" className="hover:text-white transition">
-            Tutorial
-          </Link> */}
-          {/* <Link href="/changelog" className="hover:text-white transition">
-            Changelog
-          </Link> */}
         </div>
-
-        {/* Right side buttons */}
-        {/*
-        <div className="hidden md:flex space-x-4 items-center">
-          <SignedOut>
-            <SignInButton>
-              <button
-                type="button"
-                className="bg-linear-to-r from-purple-700 to-teal-500 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 hover:from-blue-700 hover:to-pink-700 transition"
-              >
-                Login
-              </button>
-            </SignInButton>
-            <SignUpButton>
-              <button className="bg-linear-to-r from-purple-700 via-teal-600 to-blue-950 text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 hover:from-purple-700 hover:via-red-700 hover:to-pink-700 transition">
-                Create Account
-              </button>
-            </SignUpButton>
-          </SignedOut>
-
-          <SignedIn>
-            <div className="bg-linear-to-br rounded-sm from-purple-500 via-blue-500 to-purple-500  hover:from-purple-400 hover:via-blue-600 hover:to-pink-500 transition pt-1 px-1 absolute">
-              <UserButton />
-            </div>
-          </SignedIn>
-        </div> */}
 
         {/* Mobile Menu Toggle */}
         <button
@@ -98,42 +66,6 @@ export default function Navbar() {
           >
             Pricing
           </Link>
-          {/* <Link
-            href="/tutorial"
-            className="hover:text-white transition"
-            onClick={() => setIsOpen(false)}
-          >
-            Tutorial
-          </Link>
-          <Link
-            href="/changelog"
-            className="hover:text-white transition"
-            onClick={() => setIsOpen(false)}
-          >
-            Changelog
-          </Link> */}
-
-          {/* <div className="flex flex-col gap-3 pt-4 border-t border-gray-700">
-            <SignedOut>
-              <SignInButton>
-                <button className="bg-gradient-to-r from-purple-700 to-teal-500 text-white rounded-full font-medium text-base h-10 px-4 hover:from-blue-700 hover:to-pink-700 transition">
-                  Login
-                </button>
-              </SignInButton>
-
-              <SignUpButton>
-                <button className="bg-gradient-to-r from-purple-700 via-teal-600 to-blue-950 text-white rounded-full font-medium text-base h-10 px-4 hover:from-purple-700 hover:via-red-700 hover:to-pink-700 transition">
-                  Create Account
-                </button>
-              </SignUpButton>
-            </SignedOut>
-
-            <SignedIn>
-              <div className="bg-linear-to-br from-purple-500 via-blue-500 to-green-500 p-1 rounded-[10px] hover:from-purple-400 hover:via-blue-600 hover:to-pink-500 transition w-10 h-10">
-                <UserButton />
-              </div>
-            </SignedIn> 
-          </div> */}
         </div>
       </div>
     </header>
