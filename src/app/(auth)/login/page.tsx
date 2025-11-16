889d66eb246efbab8eb70e0c8e34b79b163f93c3
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-11-16 22:19:13 +0530

    ....

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,8 +1,8 @@
-import { LoginForm } from "@/components/my/login-form";
+import { LoginForm } from "@/components/my/login-form"; // My Remove
 import { SignIn } from "@clerk/nextjs";
 export default function LoginPage() {
   return (
-    <div className="text-white dark bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="text-white dark bg-purple-500 flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
         <SignIn routing="hash" />
       </div>
