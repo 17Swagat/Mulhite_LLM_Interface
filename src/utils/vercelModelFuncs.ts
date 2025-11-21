85bb01b0bbff7fc1eab7387f017237ef7dbc39fb
SWAGAT KUMAR BARUAH <17swagat@gmail.com>
2025-11-21 21:26:51 +0530

    ....

--- src/utils/vercelModelFuncs.ts
+++ src/utils/vercelModelFuncs.ts
@@ -16,7 +16,13 @@ export async function getVercelAvailableModels() {
             'deepseek/',
 
             // OpenAI Models:
-            'openai/'
+            'openai/',
+
+            'minimax/minimax-m2',
+
+            'google/',
+
+            'anthropic/'
         ]
         if (selectedModels.some((modelName) => model.id.includes(modelName))) {
             // console.log(model)
@@ -25,12 +31,21 @@ export async function getVercelAvailableModels() {
             }
         }
     }).map((model): [typeof model, string] => {
-        const termsIncludes = ['reasoning', 'thinking', 'R1', 'r1']
-        if (termsIncludes.some((term) => model.description?.includes(term))) {
+        const termsIncludes = ['reasoning', 'thinking', 'R1', 'r1', 'think', 'minimax/minimax-m2']
+
+        if (termsIncludes.some((term) => (model.description?.includes(term)) || (model.id.includes(term)))) {
             return [model, 'reasoning']
         } else {
             return [model, 'standard']
         }
+    }).filter(([model, tag]) => {
+        // 📌 Models to EXCLUDE
+        const excludeModels = ['openai/gpt-oss-20b', 'openai/gpt-5-nano', 'openai/gpt-5-codex', 'google/gemini-2.5-flash-image', 'google/gemini-2.5-flash-image-preview']
+        const excludeProviders = ['azure']
+        if (!excludeModels.includes(model.id) && !excludeProviders.includes(model.specification.provider)) {
+            return [model, tag]
+        }
+
     })
 
 
